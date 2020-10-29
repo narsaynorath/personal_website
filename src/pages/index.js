b5b3b2c9491c94ab2089a9ee86b797ac38062265
Nar Saynorath <nar.saynorath@gmail.com>
2020-10-29 00:55:08 -0400

    Update empty message

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,11 +14,8 @@ const BlogIndex = ({ data, location }) => {
       <Layout location={location} title={siteTitle}>
         <SEO title="All posts" />
         <Bio />
-        <p>
-          No blog posts found. Add markdown posts to "content/blog" (or the
-          directory you specified for the "gatsby-source-filesystem" plugin in
-          gatsby-config.js).
-        </p>
+        <p>No blog posts found.</p>
+        <p>This is the start of something beautiful.</p>
       </Layout>
     )
   }
